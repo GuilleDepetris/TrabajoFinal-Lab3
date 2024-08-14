5e34f751d705ce89e73487e6b0cd6c7955d5ab36
guilledepetris <129342778+GuilleDepetris@users.noreply.github.comse>
2024-08-14 20:19:16 -0300

    Cambio de nombre a una vista

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import PrecioList from "../views/PrecioList.vue";
 import SigIn from "../views/SigIn.vue";
 import Transacciones from "@/views/Transacciones.vue";
-import Movimientos from "../views/Movimientos.vue";
+import MovimientosList from "../views/MovimientosList.vue";
 import AnalisisEstado from "@/views/analisisEstado.vue";
 import AnalisisDeInversion from "../views/analisisDeInversion.vue";
 
@@ -25,7 +25,7 @@ const routes = [
   {
     path: "/movimientos",
     name: "Movimientos",
-    component: Movimientos,
+    component: MovimientosList,
   },
   {
     path: "/analisisEstado",
